eabd0e00ce614d958129d656e7d1398cf440125b
Lennert Vermote <lennert.vermote@linkit.nl>
2024-02-06 10:46:11 +0100

    feat: add case class experiment

--- OOP/simple-class/src/main/scala/basic/BankAccount.scala
+++ OOP/simple-class/src/main/scala/basic/BankAccount.scala
@@ -3,14 +3,27 @@ package basic
 
 // Manual Class
 class BankAccount(val accountHolder: String, val balance: Int = 0) {
-  def add(amount: Int) = new BankAccount(accountHolder, amount)
-  def remove(amount: Int) = new BankAccount(accountHolder, balance - amount)
-  def copy = new BankAccount(accountHolder, balance) // Copy all
-  def transferAccount(newAccountHolder: String) = new BankAccount(newAccountHolder, balance) // Copy balance, adjust holder
-  def adjustBalance(newBalance: Int) = new BankAccount(accountHolder, newBalance) // Copy holder, adjust balance
+  def add(amount: Int): BankAccount = new BankAccount(accountHolder, balance + amount)
+
+  def remove(amount: Int): BankAccount = new BankAccount(accountHolder, balance - amount)
+
+  def copy: BankAccount = new BankAccount(accountHolder, balance) // Copy all
+
+  def transferAccount(newAccountHolder: String): BankAccount = new BankAccount(newAccountHolder, balance) // Copy balance, adjust holder
+  def adjustBalance(newBalance: Int): BankAccount = new BankAccount(accountHolder, newBalance) // Copy holder, adjust balance
 }
 
 // Case class
+case class CaseBankAccount(accountHolder: String, balance: Int = 0) {
+  def add(amount: Int) = CaseBankAccount(accountHolder, balance + amount)
+  def addVariant(amount: Int) = this.copy(balance = this.balance + amount)
+  def remove(amount: Int) = CaseBankAccount(accountHolder, balance - amount)
+  def removeVariant(amount: Int) = this.copy(balance = this.balance - amount)
+//  def copy: CaseBankAccount = this.copy // Already implemented in case classes
+  def transferAccount(newAccountHolder: String): CaseBankAccount = this.copy(accountHolder = newAccountHolder)
+  def adjustBalance(newBalance: Int): CaseBankAccount = this.copy(balance = newBalance)
+}
+
 
 object CaseMain {
   def main(args: Array[String]): Unit = {
@@ -30,5 +43,21 @@ object CaseMain {
     val adjustedTransferedAccount = account.transferAccount("Will").adjustBalance(1600)
     println(adjustedTransferedAccount.accountHolder, adjustedTransferedAccount.balance)
 
+    // Case
+    val caseAccount = CaseBankAccount("John")
+    println(caseAccount.add(2000).balance)
+    println(caseAccount.addVariant(2000).balance)
+    println(caseAccount.remove(300).balance)
+    println(caseAccount.removeVariant(300).balance)
+
+    val caseCopyAccount = caseAccount.copy()
+    println(caseCopyAccount == caseAccount)
+    println(caseCopyAccount.accountHolder, caseCopyAccount.balance, caseAccount.accountHolder, caseAccount.balance)
+    val caseTransferAccount = caseAccount.transferAccount("Daniel")
+    println(caseTransferAccount.accountHolder)
+    val caseAdjustedAccount = caseAccount.adjustBalance(3000)
+    println(caseAdjustedAccount.balance)
+    val caseAdjustedTransferedAccount = caseAccount.transferAccount("Will").adjustBalance(1600)
+    println(caseAdjustedTransferedAccount.accountHolder, caseAdjustedTransferedAccount.balance)
   }
 }
